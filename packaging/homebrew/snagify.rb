27442c7eeb7b39a8dbaa39b00960d7e4f8c68edf
HarshDevelops <harsh10822@gmail.com>
2026-08-11 01:56:58 +0530

    Refresh Homebrew formula to v0.5.0
    Update packaging/homebrew/snagify.rb URLs and SHA-256 digests to
    match the v0.5.0 release tarballs in dist/checksums.txt.
    
    Refs: v0.5.0 release at https://github.com/HarshDevelops/snagify/releases/tag/v0.5.0

--- packaging/homebrew/snagify.rb
+++ packaging/homebrew/snagify.rb
@@ -1,26 +1,26 @@
 class Snagify < Formula
   desc "CLI to debug works-on-my-machine issues by detecting environment drift"
   homepage "https://github.com/HarshDevelops/snagify"
-  version "0.4.1"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/HarshDevelops/snagify/releases/download/v0.4.1/snagify_Darwin_arm64.tar.gz"
-      sha256 "232f7db92df1c563565fe4da0688e54333fc0ae7d0952a0b2630ae485e3d058e"
+      url "https://github.com/HarshDevelops/snagify/releases/download/v0.5.0/snagify_Darwin_arm64.tar.gz"
+      sha256 "1c395a9fde54711c6910f34345928162d0088f73b4e62f7b5cbce0c40973fb47"
     else
-      url "https://github.com/HarshDevelops/snagify/releases/download/v0.4.1/snagify_Darwin_x86_64.tar.gz"
-      sha256 "c5f83cca5323a539896694c331cf36aa6511e30c7c2eaa732c39358c89cfb670"
+      url "https://github.com/HarshDevelops/snagify/releases/download/v0.5.0/snagify_Darwin_x86_64.tar.gz"
+      sha256 "50d7fc4068749ce69f1042542ce799e342172bdb3eedeecef281d57b5e41498f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/HarshDevelops/snagify/releases/download/v0.4.1/snagify_Linux_arm64.tar.gz"
-      sha256 "fb0ed6c2222bc8f6c685fb3ca2199932faa59f9b80051c2488f2ccbf207c138b"
+      url "https://github.com/HarshDevelops/snagify/releases/download/v0.5.0/snagify_Linux_arm64.tar.gz"
+      sha256 "5cc1a0ebfc75370e3e1a985d3e99f2197db51c4057cf98ae85b536db81610a52"
     else
-      url "https://github.com/HarshDevelops/snagify/releases/download/v0.4.1/snagify_Linux_x86_64.tar.gz"
-      sha256 "f273a4d235f4dda7319aca18ba3fa72444c98dc44dca5971620197a0f84ab5bc"
+      url "https://github.com/HarshDevelops/snagify/releases/download/v0.5.0/snagify_Linux_x86_64.tar.gz"
+      sha256 "5a9fc0747f24490807902fc6c25c9336704f2dac2f4e8fa433e6ce7c8c03e564"
     end
   end
 
